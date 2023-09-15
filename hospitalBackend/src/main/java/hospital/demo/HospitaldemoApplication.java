ab7d0dcbb619ef80f6025d02c45d19dceac32cbd
PabloPeralta94 <pablo.peralta94@gmail.com>
2023-09-14 22:29:48 -0300

    initial commit

---
+++ hospitalBackend/src/main/java/hospital/demo/HospitaldemoApplication.java
@@ -0,0 +1,13 @@
+package hospital.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitaldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitaldemoApplication.class, args);
+	}
+
+}

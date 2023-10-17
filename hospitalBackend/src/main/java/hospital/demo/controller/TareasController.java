85e4568ed06bc89e7487a31123bf1692cc6d507d
unknown <pablo.peralta94@gmail.com>
2023-10-17 20:49:18 -0300

    correccion tareas

--- hospitalBackend/src/main/java/hospital/demo/controller/TareasController.java
+++ hospitalBackend/src/main/java/hospital/demo/controller/TareasController.java
@@ -12,12 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import hospital.demo.dto.receta.RecetaRequest;
 import hospital.demo.dto.receta.RecetaResponse;
@@ -30,6 +33,9 @@ import hospital.demo.security.entity.Usuario;
 import hospital.demo.security.service.UsuarioService;
 import hospital.demo.service.TareaService;
 
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1/tareas")
 public class TareasController {
 
     private final TareaService tareaService;
---
+++ hospitalFrontend/src/app/interfaces/tarea.ts
@@ -0,0 +1,13 @@
+export class Tarea {
+   tareaId: number;
+   nombre: string;
+   fechaInicio: Date | null;
+   fechaFin: Date | null;
+   enfermeraAsignada: string;
+   estado: string;
+   prioridad: string;
+   pacienteAsociado: string;
+   ubicacion: string;
+   notas: string;
+   usuarioId: number;
+}

30915fcd43c0e0979d6966e794ab9f73081adf9e
unknown <pablo.peralta94@gmail.com>
2023-10-20 16:33:53 -0300

    tareas testeadas

--- hospitalFrontend/src/app/pages/area-enfermeria/ver-tareas/ver-tareas.component.ts
+++ hospitalFrontend/src/app/pages/area-enfermeria/ver-tareas/ver-tareas.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Tarea } from 'src/app/interfaces/tarea';
+import { TareasService } from 'src/app/services/tareas.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-ver-tareas',
   templateUrl: './ver-tareas.component.html',
   styleUrls: ['./ver-tareas.component.css']
 })
-export class VerTareasComponent {
+export class VerTareasComponent implements OnInit, OnDestroy {
+  public tareas: Tarea[] = [];
+  isAdmin = false;
 
+  constructor(
+    private tareaService: TareasService,
+    private tokenService: TokenService
+  ) { }
+
+  ngOnInit() {
+    this.getTareas();
+    this.isAdmin = this.tokenService.isAdmin();        
+  }
+
+  ngOnDestroy() {   
+  }
+
+  public getTareas(): void {
+    this.tareaService.getTareas().subscribe(
+      (response: Tarea[]) => {
+        this.tareas = response.sort((a, b) => b.tareaId! - a.tareaId!);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }

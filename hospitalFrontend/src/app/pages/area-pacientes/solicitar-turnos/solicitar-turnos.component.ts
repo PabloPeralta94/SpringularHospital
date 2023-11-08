ccdba6a0eb20fb13bfbbb6c2453d6c429da36142
unknown <pablo.peralta94@gmail.com>
2023-11-08 19:14:14 -0300

    inputs en front

--- hospitalFrontend/src/app/pages/area-pacientes/solicitar-turnos/solicitar-turnos.component.ts
+++ hospitalFrontend/src/app/pages/area-pacientes/solicitar-turnos/solicitar-turnos.component.ts
@@ -8,6 +8,23 @@ import { Turno } from 'src/app/interfaces/turno';
   styleUrls: ['./solicitar-turnos.component.css']
 })
 export class SolicitarTurnosComponent {
+consultorio: string = "";
+paciente: string = "";
+body: string = "";
+
+
+
+
+
+consultorioPreview(event: any) {
+  this.consultorio = event.target.value;
+}
+bodyPreview(event: any) {
+  this.body = event.target.value;
+}
+pacientePreview(event: any) {
+  this.paciente = event.target.value;
+}
   turnoConsultorio: string;
   turnoBody: string;
   turnoPaciente: string;
@@ -37,4 +54,7 @@ export class SolicitarTurnosComponent {
       }
     );
   }
+
+  
+
 }

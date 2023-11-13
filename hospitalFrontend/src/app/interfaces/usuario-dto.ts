e961e33f797ed4b1831f1207e32220dac98a5a6d
unknown <pablo.peralta94@gmail.com>
2023-11-12 23:14:43 -0300

    cambios esteticos

---
+++ hospitalFrontend/src/app/interfaces/usuario-dto.ts
@@ -0,0 +1,5 @@
+export class UsuarioDto {
+    id: number;
+    nombre: string;
+    nombreUsuario: string;      
+}
--- hospitalFrontend/src/app/services/usuarios.service.ts
+++ hospitalFrontend/src/app/services/usuarios.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UsuarioDto } from '../interfaces/usuario-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+  private baseUrl = 'http://localhost:8080/api/v1/empleados'; 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllUsuarios(): Observable<UsuarioDto[]> {
+    return this.http.get<UsuarioDto[]>(`${this.baseUrl}`);
+  }
+
+
 }
